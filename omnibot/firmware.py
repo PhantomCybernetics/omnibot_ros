3da1606f0d47073f7a10ad3f8af44840dfbd96ff
Mirek Burkon <mirek@phntm.io>
2023-11-15 04:17:27 -0800

    drivimg both motors with one ros message

--- omnibot/firmware.py
+++ omnibot/firmware.py
@@ -6,10 +6,9 @@ import serial
 
 class OmnibotFirmware(Node):
 
-    def __init__(self):
+    def __init__(self, ser):
         super().__init__('omnibot_firmware')
-        
-        ser = serial.Serial('/dev/tty.usbserial-0001')  # open serial port to esp32
+        self.ser = ser
         
         self.subscription = self.create_subscription(
             Float32MultiArray,
@@ -19,13 +18,28 @@ class OmnibotFirmware(Node):
         self.subscription  # prevent unused variable warning
 
     def motors_cmd_callback(self, msg):
-        self.get_logger().info('Got motors cmd: "%s"' % msg.data)
-
+        # self.get_logger().info('Got motors cmd: "%s"' % msg.data)
+        if not msg.data or len(msg.data) < 2:
+            self.get_logger().err('Got invalid motors cmd: "%s"' % msg.data)
+            return
+        left_amount = int(msg.data[0])
+        right_amount = int(msg.data[1])
+        self.get_logger().info(f'Writing motors L={left_amount} R={right_amount}')
+        self.ser.write(bytes(f'm:{left_amount};{right_amount}', 'utf-8'))
+        line = self.ser.readline()
+        print(line)
 
 def main(args=None):
+    
+    ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)  # open serial port to esp32
+    print(f'Serial open: {ser.name}')
+    ser.write(b'a')
+    line = ser.readline()
+    print(line)
+    
     rclpy.init(args=args)
 
-    fw_node = OmnibotFirmware()
+    fw_node = OmnibotFirmware(ser)
 
     rclpy.spin(fw_node)
 
@@ -34,6 +48,9 @@ def main(args=None):
     # when the garbage collector destroys the node object)
     fw_node.destroy_node()
     rclpy.shutdown()
+    
+    ser.write(b'd')
+    ser.close()
 
 
 if __name__ == '__main__':
